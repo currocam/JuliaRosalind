d3d38e21c56e4cb36ea6a386303867e6976ea4c0
Curro Campuzano <campuzanocurro@gmail.com.com>
2023-07-14 19:07:00 +0200

    add corrections

---
+++ notebooks/Error_Correction_in_Reads.jl
@@ -0,0 +1,75 @@
+using BioAlignments
+using BioSequences
+using FASTX
+using Markdown
+using Test
+using BenchmarkTools
+md"""
+Given: A collection of up to 1000 reads of equal length (at most 50 bp) in FASTA format. Some of these reads were generated with a single-nucleotide error. For each read s in the dGiven: A collection of up to 1000 reads of equal length (at most 50 bp) in FASTA format. Some of these reads were generated with a single-nucleotide error. For each read s in the dataset, one of the following applies: 
+
+- s was correctly sequenced and appears in the dataset at least twice (possibly as a reverse complement);
+
+- s is incorrect, it appears in the dataset exactly once, and its Hamming distance is 1 with respect to exactly one correct read in the dataset (or its reverse complement).
+
+Return: A list of all corrections in the form "[old read]->[new read]". (Each correction must be a single symbol substitution, and you may return the corrections in any order.)ataset, one of the following applies: 
+"""
+
+function distance(s1::LongDNA, s2::LongDNA)
+  min(
+    sum(s1 .!= s2),
+    sum(reverse_complement(s1) .!= s2)
+  )
+end
+
+function distance_matrix(sequences)
+  n = length(sequences)
+  D = zeros(Int, n, n)
+  # Fill upper matrix
+  for i in 1:n
+    for j in i+1:n
+      if i != j
+        D[i, j] = D[j, i] = distance(sequences[i], sequences[j])
+      end
+    end
+  end
+  D
+end
+
+test_sequences = [
+  dna"TCATC",
+  dna"TTCAT",
+  dna"TCATC",
+  dna"TGAAA",
+  dna"GAGGA",
+  dna"TTTCA",
+  dna"ATCAA",
+  dna"TTGAT",
+  dna"TTTCC"
+]
+
+function corrected_reads(sequences)
+  D = distance_matrix(sequences)
+  duplicates = [x.I[1] for x in findall(D .== 0) |> filter(x -> x[1] > x[2])]
+  corrections = Dict{LongDNA,LongDNA}()
+  for snp in findall(D .== 1) |> filter(x -> x[1] in duplicates)
+    if sum(sequences[snp.I[2]] .!= sequences[snp.I[1]]) == 1
+      corrections[sequences[snp.I[2]]] = sequences[snp.I[1]]
+    else
+      corrections[sequences[snp.I[2]]] = reverse_complement(sequences[snp.I[1]])
+    end
+  end
+  corrections
+end
+
+expected = Dict(dna"TTCAT" => dna"TTGAT", dna"GAGGA" => dna"GATGA", dna"TTTCC" => dna"TTTCA")
+@test expected == corrected_reads(test_sequences)
+
+sequences = [LongDNA{2}(FASTX.sequence(record)) for record in open(FASTA.Reader, "datasets/rosalind_corr.txt")]
+@time corrections = corrected_reads(sequences)
+open("outputs/rosalind_corr.txt", "w") do io
+  for old in keys(corrections)
+    write(io, "$(old)->$(corrections[old])\n")
+  end
+end
+
+@benchmark corrected_reads(sequences)
